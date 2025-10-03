6b9c0344181216ace3738990b64532bb81e4392d
Swarali Gurjar <gurjar.swarali@gmail.com>
2025-10-03 11:07:41 +0530

    EX 15 Program 02.cpp

---
+++ EX 15 Program 02.cpp
@@ -0,0 +1,18 @@
+//Sum of Integers till n
+#include<iostream>
+using namespace std;
+int add(int n){
+    if(n<=1){ 
+        return 1;
+    }else{
+        return n+add(n-1); 
+    }
+}
+
+int main(){
+    int a,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    a=add(n); 
+    cout<<a;
+}
